c8dcf3159f8663fae14e06259cbaa923ba277613
Fan Yang <missdeer@dfordsoft.com>
2020-03-09 09:32:46 +0800

    (*)run as root on mac

--- main.cpp
+++ main.cpp
@@ -1,18 +1,80 @@
 #include <QApplication>
 #include <QDir>
+#include <QMessageBox>
 #include <QTranslator>
 #include <QtCore>
 
+#ifdef Q_OS_MAC
+#    include <Authorization.h>
+
+#    include <mach-o/dyld.h>
+#endif
+
 #include "mainwindow.h"
+
+#ifdef Q_OS_MAC
+bool relaunch_as_root()
+{
+    char             appPath[2048] = {0};
+    uint32_t         size          = sizeof(appPath) / sizeof(appPath[0]);
+    AuthorizationRef authRef;
+    OSStatus         status;
+
+    /* Get the path of the current program */
+    if (_NSGetExecutablePath(appPath, &size) != 0)
+    {
+        QMessageBox::critical(
+            nullptr, QObject::tr("Critial error"), QObject::tr("Could not get program path to elevate privileges."), QMessageBox::Ok);
+        return false;
+    }
+
+    status = AuthorizationCreate(NULL, kAuthorizationEmptyEnvironment, kAuthorizationFlagDefaults, &authRef);
+
+    if (status != errAuthorizationSuccess)
+    {
+        QMessageBox::critical(nullptr, QObject::tr("Critial error"), QObject::tr("Failed to create authorization reference."), QMessageBox::Ok);
+        return false;
+    }
+    status = AuthorizationExecuteWithPrivileges(authRef, appPath, kAuthorizationFlagDefaults, NULL, NULL);
+    AuthorizationFree(authRef, kAuthorizationFlagDestroyRights);
+
+    if (status == errAuthorizationSuccess)
+    {
+        /* We've successfully re-launched with root privs. */
+        return true;
+    }
+
+    return false;
+}
+#endif
 
 int main(int argc, char *argv[])
 {
+#ifndef Q_OS_WIN
+    QCoreApplication::setSetuidAllowed(true);
+#endif
     QCoreApplication::setOrganizationName(QLatin1String("https://github.com/missdeer"));
     QCoreApplication::setApplicationName(QObject::tr("CoreDNS GUI"));
     QCoreApplication::setApplicationVersion(QLatin1String("1.0"));
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QApplication a(argc, argv);
+
+    //! Commented for debug convenience.
+    //#ifdef Q_OS_MAC
+    //    if (geteuid() != 0)
+    //    {
+    //        if (relaunch_as_root())
+    //        {
+    //            /* We have re-launched with root privs. Exit this process. */
+    //            return 0;
+    //        }
+
+    //        QMessageBox::critical(
+    //            nullptr, QObject::tr("Critial error"), QObject::tr("This program requires root privileges to fully function."), QMessageBox::Ok);
+    //        return -1;
+    //    }
+    //#endif
 
     QString     locale = QLocale::system().name();
     QTranslator translator;
